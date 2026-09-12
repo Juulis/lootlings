e5f8ac0dc78141e2dc99c934ec9d2dc140e66a9e
Juulis <danny.p@live.se>
2026-09-13 01:24:12 +0200

    Återställ main.js till fungerande loop

--- js/main.js
+++ js/main.js
@@ -1,14 +1,1 @@
-import { CLASSES, createHero, equippedBonus, applyLevelUp } from "./classes.mjs";
-import { lootFromKill, compareItems } from "./loot.mjs";
-import { dist, moveTowards, enemyStats, inRange, nearestTarget, readMoveVector, isAttackHeld, shouldSwing, applyMove } from "./combat.mjs";
-import {
-  bakeSprite,
-  drawDungeon,
-  drawHpBar,
-  drawLootIcon,
-  drawPortal,
-  drawSprite,
-  frameFor,
-  SLOT_SPRITES,
-} from "./sprites.mjs";
-import { poseFrame } from "./pose.mjs";
+RESTORE_FROM_MAIN
