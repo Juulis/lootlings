9c111ea488f49ec6f38785b7b0ed99f9f9bbe353
Juulis <danny.p@live.se>
2026-09-13 02:07:39 +0200

    SFX data-moduler del 1

---
+++ js/sfx-data.mjs
@@ -0,0 +1,14 @@
+import bag from "./sfx/bag.mjs";
+import click from "./sfx/click.mjs";
+import die from "./sfx/die.mjs";
+import hit from "./sfx/hit.mjs";
+import kill from "./sfx/kill.mjs";
+import level from "./sfx/level.mjs";
+import loot from "./sfx/loot.mjs";
+import magic from "./sfx/magic.mjs";
+import portal from "./sfx/portal.mjs";
+import slash from "./sfx/slash.mjs";
+import swing from "./sfx/swing.mjs";
+export const SFX_DATA = {
+  bag, click, die, hit, kill, level, loot, magic, portal, slash, swing,
+};
